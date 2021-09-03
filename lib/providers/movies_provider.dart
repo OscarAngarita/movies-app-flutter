42db6c319f194217e2508b2bd8dd633b45741b5c
OscarAngarita <oscarangarita05@gmail.com>
2021-09-02 22:03:08 -0500

    Added: data models for movies and now playing response

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'package:movies_app/models/models.dart';
+
 class MoviesProvider extends ChangeNotifier{
+
+  String _baseUrl = 'api.themoviedb.org';
+  String _apiKey = '88f1a27550ba84026d2e13f2dadc37cb';
+  String _language = 'es-ES';
+
 
   MoviesProvider() { //Class constructor
     print('MoviesProvider inicializado');
@@ -9,7 +19,16 @@ class MoviesProvider extends ChangeNotifier{
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key' : _apiKey,
+      'language' : _language,
+      'page' : '1'
+    });
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    print(nowPlayingResponse.results[0].title);
   }
 
 }
